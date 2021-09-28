e64d7ca40f5073278f2397a6000dfb64f3444835
vinhchu <bobololo030@gmail.com>
2021-09-28 17:44:26 +0700

    update

--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -16,4 +16,21 @@ jQuery(document).ready(function ($) {
             prevEl: '.swiper-button-prev',
         },
     });
+
+    const swiper2 = new Swiper('#slider2', {
+        // Optional parameters
+        lazy: true,
+        loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 });
